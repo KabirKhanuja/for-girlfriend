885c9acbc5d87db57f57488484a847c1f2e729e0
KabirKhanuja <kabirkhanuja@gmail.com>
2025-10-02 01:12:38 +0530

    For love

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+
+export default function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main className ="page">
+      <div className="card">
+      <h1>Hey love</h1>
+      <p className ="message"> I just wanted to say: </p>
+      <div className="heart">❤️</div>
+      <h2 className="i love"> I LOVE YOU </h2>
+      <p className="small"> from kabir hehe </p>
+      <button className="btn" onClick={() => alert("You made my day💝")}>Click Me</button>
+      </div>
+      <footer>Made with ❤️ by Kabir</footer>
+    </main>
+  )
 }
 
-export default App;
+
