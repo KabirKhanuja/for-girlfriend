7f3cbdad67338fed32034ead59687d132c9400ba
KabirKhanuja <kabirkhanuja@gmail.com>
2025-10-02 01:16:47 +0530

    wrote a message

--- src/App.js
+++ src/App.js
@@ -6,13 +6,17 @@ export default function App(){
     <main className ="page">
       <div className="card">
       <h1>Hey love</h1>
-      <p className ="message"> I just wanted to say: </p>
+      <p className ="message"> 
+
+        i love MY GF cause she makes me feel at home, she's the best, she listens to my music without getting bored and she cares for me so much, she is the best what more can i say about her, the way she looks at me while i am singing for her is the best and also she gives me kisses so i like that, also she is so super understanding which is super underrated in todays time and she is the best
+        
+        I just wanted to say: </p>
       <div className="heart">❤️</div>
       <h2 className="i love"> I LOVE YOU </h2>
       <p className="small"> from kabir hehe </p>
       <button className="btn" onClick={() => alert("You made my day💝")}>Click Me</button>
       </div>
-      <footer>Made with ❤️ by Kabir</footer>
+      <footer>Made with love by Kabir</footer>
     </main>
   )
 }
